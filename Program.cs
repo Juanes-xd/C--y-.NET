11d46224660c0595ce7830d6d5edd7a10d6eb998
Juanes-xd <juanestebanortizbejarano@gmail.com>
2024-03-19 23:37:07 -0500

    variables

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿using System;
-Console.WriteLine("Hello, World!");
+
+namespace CsHelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            //Hola mundo en c#
+            Console.WriteLine("Hello, World!");
+
+            string MyString = "que pasa?";
+            Console.WriteLine(MyString);
+
+            int number = 7;
+            Console.WriteLine(number);
+
+            bool MyBool = false;
+
+            double MyDouble = 8.5;
+
+            float MyFloat = 7.3f;
+            Console.WriteLine(MyFloat);
+        }
+    }
+
+}
+
--- obj/Debug/net8.0/C#.NET.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92bcbed7098e6572dee913e06ccff3a67d61541f")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
